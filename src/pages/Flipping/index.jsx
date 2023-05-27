23c2976ec3d19934c8155574a7180f58a887dc38
kay-dev-uk <kay.dev.uk@gmail.com>
2023-05-27 23:45:45 +0100

    structre +

---
+++ src/pages/Flipping/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Flipping = () => {
+  return (
+    <div>Flipping</div>
+  )
+}
+
+export default Flipping
---
+++ src/pages/Florin/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Florin = () => {
+  return (
+    <div>Florin</div>
+  )
+}
+
+export default Florin
