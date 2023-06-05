1df0954a5c6270dd9f6de09aede1376a2314234e
kay-dev-uk <kay.dev.uk@gmail.com>
2023-06-05 14:30:36 +0100

    fix bug

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -33,10 +33,10 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div id='bot' class="flex items-center justify-center h-screen">
-        <div class="grid grid-cols-3 gap-8 m-20 max-h-[50vh]">
+      <div id='bot' class="flex items-center justify-center h-80">
+        <div class="grid grid-cols-3 gap-8 m-20">
           <Link to='/work/flipping'>
-            <div class="relative bg-white rounded-xl shadow-lg p-0 overflow-hidden transform transition-all hover:scale-105 hover:shadow-red hover:cursor-pointer hover:bg-gray-800 hover:text-white">
+            <div class="bg-white rounded-xl shadow-lg p-0 transition-all hover:scale-105 hover:shadow-red hover:cursor-pointer hover:bg-gray-800 hover:text-white">
               <img src="https://res.cloudinary.com/dlxcjxezc/image/upload/v1681999938/Screenshot_2023-04-20_at_12.52.57_c4nnq6.png" alt="Image 1" class="w-full rounded-t-lg" />
               <div className='p-5 flex-grow text'>
                 <h3 class="text-xl font-bold pb-2">Kanban AI</h3>
@@ -45,7 +45,7 @@ const Home = () => {
             </div>
           </Link>
           <Link to='/work/flipping'>
-            <div class="relative bg-white rounded-xl shadow-lg p-0 overflow-hidden transform transition-all hover:scale-105 hover:shadow-red hover:cursor-pointer hover:bg-gray-800 hover:text-white">
+            <div class="bg-white rounded-xl shadow-lg p-0 transition-all hover:scale-105 hover:shadow-red hover:cursor-pointer hover:bg-gray-800 hover:text-white">
               <img src="https://res.cloudinary.com/dlxcjxezc/image/upload/v1682340147/Flipping-2_xu7xja.png" alt="Image 2" class="w-full rounded-t-lg" />
               <div className='p-5 flex-grow text'>
                 <h3 class="text-xl font-bold pb-2">Flipping Flashcards</h3>
@@ -54,7 +54,7 @@ const Home = () => {
             </div>
           </Link>
           <Link to='/work/florin'>
-            <div class="relative bg-white rounded-xl shadow-lg p-0 overflow-hidden transform transition-all hover:scale-105 hover:shadow-red hover:cursor-pointer hover:bg-gray-800 hover:text-white">
+            <div class="bg-white rounded-xl shadow-lg p-0 transition-all hover:scale-105 hover:shadow-red hover:cursor-pointer hover:bg-gray-800 hover:text-white">
               <img src="https://res.cloudinary.com/dlxcjxezc/image/upload/v1681987403/Florin-1_h0ef8h.png" alt="Image 3" class="w-full rounded-t-lg" />
               <div className='p-5 flex-grow text'>
                 <h3 class="text-xl font-bold pb-2">Florin Events</h3>
@@ -64,15 +64,6 @@ const Home = () => {
           </Link>
         </div>
       </div>
-        <div className="flex items-center justify-center h-20">
-          EMPTY
-        </div>
-        <div className="flex items-center justify-center h-20">
-          EMPTY
-        </div>
-
-
-
     </>
   )
 }
