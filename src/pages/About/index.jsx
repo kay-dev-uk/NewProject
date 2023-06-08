3e49157213d5134038bb92caf27a7b18a90363bc
kay-dev-uk <kay.dev.uk@gmail.com>
2023-06-08 23:49:26 +0100

    Fix fixable

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -20,7 +20,7 @@ const About = () => {
           </div>
         </div>
       </div> */}
-      <div className="flex items-center justify-center h-screen">
+      {/* <div className="flex items-center justify-center h-screen">
         <div className="bg-white rounded-xl shadow-custom m-20 p-5">
           <div className="aspect-w-16 aspect-h-9 p-5">
             <iframe
@@ -33,38 +33,48 @@ const About = () => {
             ></iframe>
           </div>
         </div>
-      </div>
-      <div className="flex items-center justify-center">
-      <div className="bg-white rounded-xl shadow-custom m-20 p-5">
-        <div className="flex">
-          <div className="mx-2">
-            <div>
-              <h1>About Me</h1>
-              <p>As a multilingual professional, I have developed a unique skill set by working across various industries such as customer service, logistics, and management. My passion for technology and dedication to delivering exceptional customer experiences have been the driving forces throughout my career.</p>
-              <p>Recognising the potential for growth and impact in the tech industry, I made a bold decision to pursue a career in Tech leaving my full-time job last year and enrolling in a full-time intensive programming bootcamp. This investment not only demonstrates my commitment to the world of technology but also allowed me to further develop my skills and potential.</p>
-              <p>Armed with a Bachelor's degree in International Relations and newfound programming expertise, I am now well-prepared to take on the challenges of the tech industry. As a valuable asset to any organisation, I am eager to embark on my next professional adventure, using my diverse skills and experiences to make a meaningful difference in the fast-paced, ever-evolving world of technology.</p>
+      </div> */}
+        <div className="flex items-center justify-center">
+          <div className="bg-white rounded-xl shadow-custom m-20 p-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
+              <div className='order-2 md:order-2'>
+                <h1>About Me</h1>
+                <p>As a multilingual professional, I have developed a unique skill set by working across various industries such as customer service, logistics, and management. My passion for technology and dedication to delivering exceptional customer experiences have been the driving forces throughout my career.</p>
+                <p>Recognising the potential for growth and impact in the tech industry, I made a bold decision to pursue a career in Tech leaving my full-time job last year and enrolling in a full-time intensive programming bootcamp. This investment not only demonstrates my commitment to the world of technology but also allowed me to further develop my skills and potential.</p>
+                <p>Armed with a Bachelor's degree in International Relations and newfound programming expertise, I am now well-prepared to take on the challenges of the tech industry. As a valuable asset to any organisation, I am eager to embark on my next professional adventure, using my diverse skills and experiences to make a meaningful difference in the fast-paced, ever-evolving world of technology.</p>
+              </div>
+              <div className="md:flex md:items-center order-1 md:order-2">
+                <img src="https://res.cloudinary.com/dlxcjxezc/image/upload/v1682430307/IMG_9755_e6ss8x.jpg" alt="Main Image" className="rounded-lg mx-auto my-4 md:my-0 max-w-full" />
+              </div>
+            </div>
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 mt-5">
+            <div className="flex-1 bg-gray-200 rounded-lg p-4 mx-1">
+              <h2>Enthusiastic</h2>
+              <p>During my time as TL I did that. During my time as TL I did that.During my time as TL I did that.</p>
+            </div>
+            <div className="flex-1 bg-gray-200 rounded-lg p-4 mx-1">
+              <h2>Enthusiastic</h2>
+              <p>During my time as TL I did that. During my time as TL I did that.During my time as TL I did that.</p>
+            </div>
+            <div className="flex-1 bg-gray-200 rounded-lg p-4 mx-1">
+              <h2>Enthusiastic</h2>
+              <p>During my time as TL I did that. During my time as TL I did that.During my time as TL I did that.</p>
+            </div>
+            <div className="flex-1 bg-gray-200 rounded-lg p-4 mx-1">
+              <h2>Enthusiastic</h2>
+              <p>During my time as TL I did that. During my time as TL I did that.During my time as TL I did that.</p>
+            </div>
+            <div className="flex-1 bg-gray-200 rounded-lg p-4 mx-1">
+              <h2>Enthusiastic</h2>
+              <p>During my time as TL I did that. During my time as TL I did that.During my time as TL I did that.</p>
+            </div>
+            <div className="flex-1 bg-gray-200 rounded-lg p-4 mx-1">
+              <h2>Enthusiastic</h2>
+              <p>During my time as TL I did that. During my time as TL I did that.During my time as TL I did that.</p>
             </div>
           </div>
-          <div className="inline-block">
-            <img src="https://res.cloudinary.com/dlxcjxezc/image/upload/v1682430307/IMG_9755_e6ss8x.jpg" alt="Main Image" className="rounded-lg h-full w-full" />
-          </div>
-        </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-5">
-        <div className="flex-1 bg-gray-200 rounded-lg p-4 mx-1">
-          <h2>Enthusiastic</h2>
-          <p>During my time as TL I did that. During my time as TL I did that.During my time as TL I did that.</p>
-        </div>
-        <div className="flex-1 bg-gray-200 rounded-lg p-4 mx-1">
-          <h2>Enthusiastic</h2>
-          <p>During my time as TL I did that. During my time as TL I did that.During my time as TL I did that.</p>
-        </div>
-        <div className="flex-1 bg-gray-200 rounded-lg p-4 mx-1">
-          <h2>Enthusiastic</h2>
-          <p>During my time as TL I did that. During my time as TL I did that.During my time as TL I did that.</p>
         </div>
       </div>
-    </div>
-  </div>
     </>
   )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -16,7 +16,7 @@ const Home = () => {
         </div>
       </div>
 
-      <div id='mid' className="flex items-center justify-center h-screen">
+      <div id='mid' className="flex items-center justify-center h-auto">
         <div className="bg-white rounded-xl shadow-custom m-20 flex flex-col-reverse sm:flex-row items-center justify-center">
           <div className="pl-8 pr-8 pt-8 pb-8 min-w-50% max-w-50%">
             <h2 className="text-2xl font-bold pb-2">Hello and welcome to my portfolio website!</h2>
@@ -33,11 +33,11 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div id='bot' class="flex items-center justify-center h-80">
+      <div id='bot' class="flex items-center justify-center h-auto">
         <div class="grid grid-cols-3 gap-8 m-20">
           <Link to='/work/flipping'>
             <div class="bg-white rounded-xl shadow-lg p-0 transition-all hover:scale-105 hover:shadow-red hover:cursor-pointer hover:bg-gray-800 hover:text-white">
-              <img src="https://res.cloudinary.com/dlxcjxezc/image/upload/v1681999938/Screenshot_2023-04-20_at_12.52.57_c4nnq6.png" alt="Image 1" class="w-full rounded-t-lg" />
+              <img src="https://res.cloudinary.com/dlxcjxezc/image/upload/v1681999938/Screenshot_2023-04-20_at_12.52.57_c4nnq6.png" alt="Image 1" class="w-full rounded-t-lg h-[13em]" />
               <div className='p-5 flex-grow text'>
                 <h3 class="text-xl font-bold pb-2">Kanban AI</h3>
                 <p class="text-gray-400">New way to manage projects using AI tools.</p>
@@ -46,7 +46,7 @@ const Home = () => {
           </Link>
           <Link to='/work/flipping'>
             <div class="bg-white rounded-xl shadow-lg p-0 transition-all hover:scale-105 hover:shadow-red hover:cursor-pointer hover:bg-gray-800 hover:text-white">
-              <img src="https://res.cloudinary.com/dlxcjxezc/image/upload/v1682340147/Flipping-2_xu7xja.png" alt="Image 2" class="w-full rounded-t-lg" />
+              <img src="https://res.cloudinary.com/dlxcjxezc/image/upload/v1682340147/Flipping-2_xu7xja.png" alt="Image 2" class="w-full rounded-t-lg h-[13em]" />
               <div className='p-5 flex-grow text'>
                 <h3 class="text-xl font-bold pb-2">Flipping Flashcards</h3>
                 <p class="text-gray-400">An innovative and cutting-edge study tool.</p>
@@ -55,7 +55,7 @@ const Home = () => {
           </Link>
           <Link to='/work/florin'>
             <div class="bg-white rounded-xl shadow-lg p-0 transition-all hover:scale-105 hover:shadow-red hover:cursor-pointer hover:bg-gray-800 hover:text-white">
-              <img src="https://res.cloudinary.com/dlxcjxezc/image/upload/v1681987403/Florin-1_h0ef8h.png" alt="Image 3" class="w-full rounded-t-lg" />
+              <img src="https://res.cloudinary.com/dlxcjxezc/image/upload/v1681987403/Florin-1_h0ef8h.png" alt="Image 3" class="w-full rounded-t-lg h-[13em]" />
               <div className='p-5 flex-grow text'>
                 <h3 class="text-xl font-bold pb-2">Florin Events</h3>
                 <p class="text-gray-400">Event creation app made for our community.</p>
